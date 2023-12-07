14cabe4d4f6f5aa119f4df0d75f3bde93afc76df
tania <tanakorchynska@gmail.com>
2023-12-07 18:51:13 -0500

    Added bootstrap to navigation

--- my-app/src/components/Navigation/Navigation.js
+++ my-app/src/components/Navigation/Navigation.js
@@ -3,30 +3,338 @@ import "./Navigation.css";
 
 function Navigation() {
   return (
-    <nav className="Navigation">
-      <div className="navbar-logo"></div> 
-      <ul className="nav-links">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/gift-of-the-heart">Gift of Heart</a>
-        </li>
-        <li>
-          <a href="/gift-of-the-art">Gift of Art</a>
-        </li>
-        <li>
-          <a href="/people-we-love">People We Love</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-      <button className="support-button">Support</button>
-    </nav>
+    <section className="main-container">
+      <nav className="navbar navbar-expand-lg sticky-top">
+        <div className="container-fluid nav-container">
+          {/* Logo */}
+          <div className="navbar-logo"></div>
+          <div>
+            {/* Collapse toggler */}
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            {/* Nav Menu Links */}
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                {/* Home */}
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    aria-current="page"
+                    href="https://www.google.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Home
+                  </a>
+                </li>
+                {/* About */}
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="https://www.google.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    About
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Who We Are
+                        <br />
+                        <span className="dropdown-span">
+                          Our Mission & Story
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        What We Have Done
+                        <br />
+                        <span className="dropdown-span">
+                          Hospitals we have worked with.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Testimonials
+                        <br />
+                        <span className="dropdown-span">
+                          What Our supporters have to say.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Board
+                        <br />
+                        <span className="dropdown-span">
+                          Everyone who made this foundation happen.
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                {/* Gift of Heart */}
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="https://www.google.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Gift of Heart
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Heart Facts
+                        <br />
+                        <span className="dropdown-span">
+                          Sudden Cardiac Death statistics and warning signs.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Screenings
+                        <br />
+                        <span className="dropdown-span">
+                          Learn the best way to detect heart complications.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Emergencies
+                        <br />
+                        <span className="dropdown-span">
+                          How to take action in the event of an emergency.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Expert Health Tips
+                        <br />
+                        <span className="dropdown-span">
+                          Advice from professionals to live a healthy life.
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                {/* Gift of Art */}
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="https://www.google.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Gift of Art
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Scholarships
+                        <br />
+                        <span className="dropdown-span">
+                          Great opportunity for students.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Plays
+                        <br />
+                        <span className="dropdown-span">
+                          Learning experiences through storytelling and theatre.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Films
+                        <br />
+                        <span className="dropdown-span">
+                          Films with eye-opening messages.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Books
+                        <br />
+                        <span className="dropdown-span">
+                          Stories that nourish the spirit.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Upcoming Events
+                        <br />
+                        <span className="dropdown-span">
+                          Check out events to keep an eye for!
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                {/* People We Love */}
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="https://www.google.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    People We Love
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Tributes
+                        <br />
+                        <span className="dropdown-span">
+                          Honoring young lives.
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="dropdown-item"
+                        href="https://www.google.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Survivors
+                        <br />
+                        <span className="dropdown-span">
+                          Read stories from our Survivors.
+                        </span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                {/* Contact */}
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    aria-current="page"
+                    href="https://www.google.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Contact
+                  </a>
+                </li>
+                {/* Support Button */}
+                <button className="btn support-button" type="button">
+                  Support
+                </button>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </section>
   );
 }
 
