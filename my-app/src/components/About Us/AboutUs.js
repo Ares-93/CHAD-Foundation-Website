be8c57e1fc73143fc3d1bafd2ad113efd05a6f7b
Mohammad Haider <arzhdr@gmail.com>
2023-12-10 18:56:33 -0500

    Update AboutUs.js
    filling in about page

---
+++ my-app/src/components/About Us/AboutUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+const AboutUs = () => {
+  return (
+    <Container fluid>
+      <Row className="my-4">
+        <Col>
+          <h2 className="display-4">WHO WE ARE</h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default AboutUs;
