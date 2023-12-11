3296efe81077af032f1dfa139908a159dd0f168b
tania <tanakorchynska@gmail.com>
2023-12-11 17:43:49 -0500

    Changed folder structure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import HomeScreen from "./components/MainPage/HomeScreen/HomeScreen";
+import AboutUs from "./components/MainPage/AboutUs/AboutUs"
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <HomeScreen />
+      <AboutUs />
     </div>
   );
 }
