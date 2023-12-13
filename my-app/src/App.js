e6063682f4a08220ca60a7f4e92ca96391eae7e1
Mohammad Haider <arzhdr@gmail.com>
2023-12-13 00:41:53 -0500

    About us update
    added files and content to about us, still needs work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import HomeScreen from "./components/MainPage/HomeScreen/HomeScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AboutUs from "./components/About Us/AboutUs";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <HomeScreen />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Route path="/ about" component={AboutUs} />
+        <Route path="/" exact component={HomeScreen} />
+      </div>
+    </Router>
   );
 }
 
--- my-app/src/components/About Us/AboutUs.js
+++ my-app/src/components/About Us/AboutUs.js
@@ -1,14 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+
 const AboutUs = () => {
-  return (
-    <Container fluid>
-      <Row className="my-4">
-        <Col>
-          <h2 className="display-4">WHO WE ARE</h2>
-        </Col>
-      </Row>
-    </Container>
-  );
+  return <div className="about-us">{}</div>;
 };
 export default AboutUs;
