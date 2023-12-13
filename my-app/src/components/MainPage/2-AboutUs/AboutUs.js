8b19123745d073930e5b3736f292528db3db78de
Mohammad Haider <arzhdr@gmail.com>
2023-12-13 03:26:21 -0500

    made the base for the about us

--- my-app/src/components/MainPage/2-AboutUs/AboutUs.js
+++ my-app/src/components/MainPage/2-AboutUs/AboutUs.js
@@ -1,6 +1,30 @@
 import React from "react";
 
 const AboutUs = () => {
-  return <div className="about-us">{}</div>;
+  return (
+    <section className="about-story-mission">
+      <div className="Who_Are_We-container">
+        <h2 className="about-subheader">Who Are We:Our Story & Mission</h2>
+        <p className="about-text-p1">
+          {" "}
+          The Mission of The Chad Foundation for Athletes and Artists is
+          two-fold: First, to raise awareness of Sudden Cardiac Death in Young
+          Athletes and Students.
+        </p>
+        <p className="about-text-p2">
+          {" "}
+          Secondarily, as artists our mission is to create/produce/sponsor art -
+          film, theatre and books that celebrate American stories of all
+          heritages.
+        </p>
+        <img
+          className="about-us-chad"
+          src="my-app\src\images\AboutUs\Chad.jpg"
+          alt="Chad"
+        />
+        <button>Learn More</button>
+      </div>
+    </section>
+  );
 };
 export default AboutUs;
