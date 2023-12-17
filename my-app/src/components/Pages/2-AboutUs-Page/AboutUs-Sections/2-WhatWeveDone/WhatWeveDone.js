ef308725bb35039c74734676862def7c9b63f00a
tania <tanakorchynska@gmail.com>
2023-12-16 19:29:25 -0500

    minor name fixes

---
+++ my-app/src/components/Pages/2-AboutUs-Page/AboutUs-Sections/2-WhatWeveDone/WhatWeveDone.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./WhatWeveDone.css";
+
+const WhatWeveDone = () => {
+  return (
+    <section className="WhatWeveDone">
+      <h3>WhatWeveDone</h3>
+    </section>
+  );
+};
+export default WhatWeveDone;
