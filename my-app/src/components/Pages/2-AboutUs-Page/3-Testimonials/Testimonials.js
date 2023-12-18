2ed1c40a30cd73600a7c358bb9f8c72d1b2e2215
tania <tanakorchynska@gmail.com>
2023-12-18 14:39:38 -0500

    Added navigation within AboutUs page

--- my-app/src/components/Pages/2-AboutUs-Page/3-Testimonials/Testimonials.js
+++ my-app/src/components/Pages/2-AboutUs-Page/3-Testimonials/Testimonials.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./Testimonials.css";
+import AboutUsNavigation from "../AboutUsNavigation";
+
 
 const Testimonials = () => {
   return (
     <section className="Testimonials">
+      <AboutUsNavigation currentPage="testimonials" />
       <h3>Testimonials</h3>
     </section>
   );
--- my-app/src/components/Pages/2-AboutUs-Page/4-Board/Board.js
+++ my-app/src/components/Pages/2-AboutUs-Page/4-Board/Board.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Board.css";
+import AboutUsNavigation from "../AboutUsNavigation";
 
 const Board = () => {
   return (
     <section className="Board">
+      <AboutUsNavigation currentPage="board" />
       <h3>Board</h3>
     </section>
   );
