424b44753ef641a9e9aa5618907e2176aebd7594
Mohammad Haider <arzhdr@gmail.com>
2023-12-18 12:29:17 -0500

    Updated Support:, and made adjustments to the Who we are seection

--- my-app/src/components/Pages/1-Home-Page/2-WhoWeAre/WhoWeAre.js
+++ my-app/src/components/Pages/1-Home-Page/2-WhoWeAre/WhoWeAre.js
@@ -8,22 +8,24 @@ const WhoWeAre = () => {
       <div className="main-container">
         <div className="header-container">
           <h2 className="story-subheader">Who Are We: Our Story & Mission</h2>
+          <div className="text-container">
+            <p className="story-text1">
+              The mission of The Chad Foundation for Athletes and Artists is
+              two-fold: First, to raise awareness of Sudden Cardiac Death in
+              Young Athletes and Students.
+            </p>
+            <br />
+            <p className="story-text2">
+              Secondly, as artists our mission is to create/produce/sponsor art
+              - film, theatre, and books that celebrate American stories of all
+              heritages.
+            </p>
+            <button className="Learn-More-Btn">Learn More</button>
+          </div>
         </div>
-        <div className="text-container">
-          <p className="story-text">
-            The mission of The Chad Foundation for Athletes and Artists is
-            two-fold: First, to raise awareness of Sudden Cardiac Death in Young
-            Athletes and Students.
-          </p>
-          <br />
-          <p className="story-text">
-            Secondarily, as artists our mission is to create/produce/sponsor art
-            - film, theatre, and books that celebrate American stories of all
-            heritages.
-          </p>
-          <button className="Learn-More-Btn">Learn More</button>
+        <div className="image-container">
+          <img className="story-chad" src={Chad} alt="Chad's Story" />
         </div>
-        <img className="story-chad" src={Chad} alt="Chad" />
       </div>
     </section>
   );
--- my-app/src/components/Pages/1-Home-Page/7-Support/Support.js
+++ my-app/src/components/Pages/1-Home-Page/7-Support/Support.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./Support.css";
-
+import Books from "../../../../images/HomePage-Images/CHAD books.png";
 function Support() {
   return (
     <section className="Support">
       <div className="main-container">
-        <h2>hello from Support</h2>
+        <h2>Support the Chad Foundation:</h2>
+        <div className="content">
+          <div className="book-display">
+            <img className="book-chad" src={Books} alt="Book" />
+          </div>
+          <div className="text-content">
+            <h3>CHAD: A Celebration of Life Beyond a Mother’s Memories</h3>
+            <p>
+              Celebrating the lives of young athletes lost to Sudden Cardiac
+              Death, and the journey of its survivors, this uplifting memoir
+              details the 25-year history of The Chad From SCD victims to
+              survivors— from heartbreak to heroism— learn about how The Chad
+              Foundation has saved lives and shaped a better tomorrow.
+            </p>
+            <button>Support</button>
+          </div>
+        </div>
       </div>
     </section>
   );
