843e2826c50c6a45165761bb5d3db0e394f9cb8e
tania <tanakorchynska@gmail.com>
2024-01-03 12:34:54 -0500

    Added Testimonials Section

--- my-app/src/components/Pages/2-AboutUs-Page/1-WhoWeAre/6-Testimonials/Testimonials.js
+++ my-app/src/components/Pages/2-AboutUs-Page/1-WhoWeAre/6-Testimonials/Testimonials.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Testimonials.css";
+import QuotationLine from "../../../../../images/HomePage-Images/quotation-mark-line.png";
+import QuotationMark from "../../../../../images/HomePage-Images/quotation-mark.png";
 
-const Testimonials = () => {
+function Testimonials() {
+  const [isSmall, setIsSmall] = useState(window.innerWidth <= 1060);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmall(window.innerWidth <= 1060);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <section className="Testimonials">
-      <h3>Testimonials</h3>
+      <div className="main-container">
+        <h2 className="Testimonials-header">Testimonials</h2>
+        <div className="Testimonials-container">
+          <div className="Testimonials-background"></div>
+          <div className="Testimonials-content">
+            <div className="Testimonials-quotation-div">
+              {isSmall && (
+                <img
+                  src={QuotationMark}
+                  alt="quotation mark"
+                  className="quotationMark"
+                />
+              )}
+              <img
+                src={QuotationLine}
+                alt="quotation"
+                className="quotationLine-top"
+              />
+              <h2 className="Testimonials-h2">
+                My life has changed forever after that screening. I don’t know
+                where I would be without the knowledge I have about my heart.
+              </h2>
+              <img
+                src={QuotationLine}
+                alt="quotation"
+                className="quotationLine-bottom"
+              />
+              <p className="Testimonials-p">Lindsay</p>
+            </div>
+            <button className="Testimonials-button">
+              Read More Testimonials
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
-};
+}
+
 export default Testimonials;
