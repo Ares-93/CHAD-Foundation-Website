1a10175970caf46db9b42c7a01c97d0af66f51ef
tania <tanakorchynska@gmail.com>
2024-01-03 09:57:53 -0500

    Added Message Section

--- my-app/src/components/Pages/2-AboutUs-Page/1-WhoWeAre/3-Message/Message.js
+++ my-app/src/components/Pages/2-AboutUs-Page/1-WhoWeAre/3-Message/Message.js
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Message.css";
+import AristaPhoto from "../../../../../images/AboutUs-Images/arista-photo.png";
 
 const Message = () => {
   return (
     <section className="Message">
-      <h3>Message</h3>
+      <div className="main-container">
+        <h2 className="message-header">Message from the Founder</h2>
+        <div className="message-content">
+          <div className="message-text">
+            <strong>
+              “I remember reading an article at that time that said “12 young
+              athletes die annually from Sudden Cardiac Death.” Twelve??”
+            </strong>
+            <br />
+            <br />
+            I can hardly believe 21 years have passed since our “1st Chad
+            Echocardiogram Screening for Young Athletes” which took place at
+            Chad’s alma mater, North Hollywood High, CA, the first screening of
+            its kind in the nation.
+            <br />
+            <br />
+            When I first began this crusade to safeguard young hearts, it was
+            like donning my pioneer bonnet and setting off into uncharted
+            territory. <span>Read More</span>
+          </div>
+          <div className="message-photo">
+            <img src={AristaPhoto} alt="Founder" className="arista-photo" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
