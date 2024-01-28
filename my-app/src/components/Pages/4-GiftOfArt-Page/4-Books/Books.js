47066fb138ffe6e3f7b94d6d086eaa08997be69e
tania <tanakorchynska@gmail.com>
2024-01-28 11:16:22 -0500

    Added Books Section

--- my-app/src/components/Pages/4-GiftOfArt-Page/4-Books/Books.js
+++ my-app/src/components/Pages/4-GiftOfArt-Page/4-Books/Books.js
@@ -1,12 +1,44 @@
 import React from "react";
 import GiftOfArtNavigation from "../GiftOfArtNavigation";
 import './Books.css';
+import HeartIcon from "../../../../images/GiftOfArt-Images/gift-of-art-heart-icon.png";
+import BooksImage from "../../../../images/GiftOfArt-Images/books-image.png";
+import BooksVector from "../../../../images/GiftOfArt-Images/books-vector.png";
+
 
 const Books = () => {
   return (
-    <section className="books">
+    <section className="Books">
       <GiftOfArtNavigation currentPage="books" />
-      <h1>hello Books</h1>
+      <div className="main-container">
+        {/* ONE */}
+        <div className="books-content">
+          <div className="books-content-left">
+            <div className="books-header-box">
+              <img src={HeartIcon} alt="heart" width={60} height={60} />
+              <h2 className="books-header">The Gemini Kid </h2>
+            </div>
+            <div className="books-subtext-box">
+              <img src={BooksVector} alt="heart" width={14} height={20} />
+              <h4 className="books-subtext">Scholarship Opportunity</h4>
+            </div>
+            <p className="books-p">
+              THE GEMINI KIID is a lovable little character who has many
+              different parts, many different feelings, many different actions
+              and many different colours. GEMMI is the main character in the
+              children’s picture book series, The Adventures of the GEMINImKIID.
+              In the first book, The Gemini Kiid Comes To Earth! the ‘little
+              kiid’ falls to earth and is adopted by twin siblings, named Keira
+              and Kyle Laurent. Though Gemmi is loved by his new family who
+              praise his agility, his diversity and his quick mind, Gemmi is
+              also smart enough to know and see how different he is.
+            </p>
+          </div>
+          <div className="books-image-box">
+            <img src={BooksImage} alt="Book" className="books-image" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
