1ea3722564ed1c2e3df243d3063c929521cff5a0
tania <tanakorchynska@gmail.com>
2024-02-21 15:55:09 -0500

    Update Books.js

--- my-app/src/components/Pages/4-GiftOfArt-Page/4-Books/Books.js
+++ my-app/src/components/Pages/4-GiftOfArt-Page/4-Books/Books.js
@@ -1,10 +1,9 @@
 import React from "react";
 import GiftOfArtNavigation from "../GiftOfArtNavigation";
-import './Books.css';
+import "./Books.css";
 import HeartIcon from "../../../../images/GiftOfArt-Images/gift-of-art-heart-icon.png";
 import BooksImage from "../../../../images/GiftOfArt-Images/books-image.png";
 import BooksVector from "../../../../images/GiftOfArt-Images/books-vector.png";
-
 
 const Books = () => {
   return (
@@ -36,6 +35,30 @@ const Books = () => {
           </div>
           <div className="books-image-box">
             <img src={BooksImage} alt="Book" className="books-image" />
+          </div>
+        </div>
+        {/* TWO */}
+        <div className="books-content">
+          <div className="books-image-box">
+            <img src={BooksImage} alt="Book" className="books-image" />
+          </div>
+          <div className="books-content-left">
+            <div className="books-header-box">
+              <img src={HeartIcon} alt="heart" width={60} height={60} />
+              <h2 className="books-header">Chad book</h2>
+            </div>
+            <p className="books-p">
+              This uplifting memoir details the 25-year history of The Chad
+              Foundation to safeguard hearts by providing 10,000 screenings and
+              celebrates the lives of young athletes lost to Sudden Cardiac
+              Death, and the journey of survivors whose lives were saved due to
+              early intervention and detection. It is endorsed by Adam Silver,
+              Commissioner of the National Basketball Association and informs
+              how to safeguard your own heart through CPR and AED access. CHAD
+              celebrates life’s spectrum from heartbreak to heroism—“The Heart
+              is the Home of our two greatest gifts – Love and Life,” and must
+              be safeguarded as our most valued possession.
+            </p>
           </div>
         </div>
       </div>
