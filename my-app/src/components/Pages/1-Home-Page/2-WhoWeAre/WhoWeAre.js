37a44c1b66412d74509e21c9ba9fb28891647808
tania <tanakorchynska@gmail.com>
2024-03-16 15:12:35 -0400

    Update WhoWeAre.js

--- my-app/src/components/Pages/1-Home-Page/2-WhoWeAre/WhoWeAre.js
+++ my-app/src/components/Pages/1-Home-Page/2-WhoWeAre/WhoWeAre.js
@@ -12,15 +12,24 @@ const WhoWeAre = () => {
         <div className="content-container">
           <div className="text-container">
             <p className="story-text">
-              The mission of The Chad Foundation for Athletes and Artists is
-              two-fold: First, to raise awareness of Sudden Cardiac Death in
-              Young Athletes and Students.
+              CHAD’s mission is dual: The Gift of Heart raises awareness of
+              Sudden Cardiac Death and Heart Anomalies in Young Athletes and
+              Students, promotes preventive heart screenings, CPR, and AEDS, and
+              advocates a “Heart-Healthy” Lifestyle through monthly diet tips
+              and Health Professional interviews.
             </p>
 
             <p className="story-text">
-              Secondly, as artists our mission is to create/produce/sponsor art
-              - film, theatre, and books that celebrate American stories of all
-              heritages.
+              The Gift Art, creates, produces, and sponsors art - film, theatre,
+              and books that celebrate stories of all heritages, addresses
+              societal issues like anti-substance abuse and bullying, and
+              provides “CHAD Annual Scholarships for the “Best
+              Scholar-Leader-Athlete,” and “Best Story of the Heart.”
+            </p>
+            <p className="story-text">
+              <strong>
+                “Healthy Body, Mind and Spirit” – How Chad Lived his Life
+              </strong>
             </p>
             <button className="Learn-More-Btn">Learn More</button>
           </div>
