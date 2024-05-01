104ef2a5c6efb5844bdbfaa162813ef6e9ecaf39
tania <tanakorchynska@gmail.com>
2024-04-30 21:15:59 -0400

    Update Message.js

--- my-app/src/components/Pages/2-AboutUs-Page/1-WhoWeAre/3-Message/Message.js
+++ my-app/src/components/Pages/2-AboutUs-Page/1-WhoWeAre/3-Message/Message.js
@@ -25,116 +25,94 @@ const Message = () => {
               Dearest Friends and Supporters,
               <br />
               <br />
-              26 years have passed since our “1st Chad Echocardiogram Screening
-              for Young Athletes” took place at Chad’s alma mater, North
-              Hollywood High, CA, the first echocardiogram screening of its kind
-              in the nation. Back then it was said{" "}
-              <strong>
-                “12 young athletes die annually from Sudden Cardiac Death.”
-                Twelve??{" "}
-              </strong>
-              The Centers of Disease Control now say 6,000 young people under
-              the age of 35 die annually from Sudden Cardiac Arrest-that’s a
-              small town-and may be a low figure with all the young deaths that
-              go unreported.
+              26 years ago, the “1st Chad Echocardiogram Screening for Young
+              Athletes” took place at Chad’s alma mater, North Hollywood High,
+              CA—the first echocardiogram screening of its kind in the nation.
+              Then it was said “12 young athletes die annually from S C D.” The
+              Centers of Disease Control now say 6,000 young people under 35 die
+              annually from Sudden Cardiac Arrest.
               {isExpanded && (
                 <>
                   <br />
                   <br />
-                  Thanks to Cardiologists and Sonographers, from Children’s
-                  Hospital Los Angeles Heart Institute, Johns Hopkins Heart
-                  Hype, Mt. Sinai Hospital, NYU Langone Hospital, Holyoke
-                  Medical Center in MA, manufacturing partners, such as Philips
-                  and SonoSite, and Friends of Chad, that first screening
+                  Thanks to Cardiologists, Sonographers, Hospitals, Mfg.
+                  partners, Volunteers and Friends of Chad, that first screening
                   flourished to 10,000 young people screened in 5 states and
                   Austria and Sweden. Chad found an average 15-20% anomaly rate
-                  for every 100 screened; resulting in surgeries and patients
-                  and families followed sometime for life. Today, Chad is only
-                  the masthead being the 1st, there is a flotilla of hundreds of
-                  screening organizations screening thousands of young hearts
-                  and saving lives. Awareness has been raised, and though heart
-                  screenings for athletes are not mandatory in the US, in Italy
-                  prescreening of their athletes is mandated by law. Their
-                  25-year stats show early detection does save lives, a 90% save
-                  rate, and Japan has instituted laws that provide hearts
-                  screenings for their school children with follow-up and
-                  education. The NBA is the 1st professional sports organization
-                  to mandate Echocardiogram screenings for their athletes.
+                  for every 100 screened, resulting in surgeries and patients
+                  and families being followed sometimes for life. Today, Chad is
+                  only the masthead; there is a flotilla of hundreds of
+                  organizations screening thousands of young hearts and saving
+                  lives. Awareness is being raised, and though screenings for
+                  athletes are not mandatory in the US, in Italy prescreening of
+                  athletes is mandated by law; 25-year stats show early
+                  detection has a 90% save rate there. Japan has laws that
+                  provide heart screenings for their school children. The NBA is
+                  the 1st professional sports organization to mandate
+                  Echocardiograms for their athletes.
                   <br />
                   <br />
-                  What’s ahead for “The Gift of Heart?” Chad will advocate for
-                  hospitals across the US to institute Heart Screening Programs
-                  for Young Athletes and Students. Several hospitals across our
-                  nation have already initiated successful heart screening
-                  programs to detect unknown anomalies. As hospitals have,
-                  state-of-art equipment and follow-up in place, it will be a
-                  nation-wide focus to enlist their support in our mutual
-                  mission to safeguard young hearts. As Heart Healthy Living
-                  starts at home. Chad will initiate a program of Heart Healthy
-                  Living showcasing recipes created by Nutritionists and High
-                  School Health and Culinary Arts Programs, featuring a Heart
-                  Healthy Monthly Recipe. Also, Doctors in a monthly forum and
-                  other Health professionals will speak about Heart Education
-                  and how awareness and prevention is vital in safeguarding your
-                  children and your own hearts. Many parents who have lost a
-                  child to SCD tirelessly advocate for lifesaving tools the AED
-                  and CPR so they are available in every school.. I invite you
-                  to join us, spread the word, become aware of your heart,
-                  support us, help us make those screenings happen in your
-                  communities nationwide. Know the Warning Signs and The Chain
-                  of Survival. Volunteerism is integral in making screening
-                  programs happen in your own community they know, ‘There is no
-                  greater gift than helping to save a young person’s Life.’
+                  <strong>What’s ahead for “The Gift of Heart?”</strong>
+                  <br />
+                  Chad will advocate for hospitals across the US to institute
+                  Heart Screening Programs for Young Athletes and Students as
+                  they have state-of-the- art equipment and follow-up in place.
+                  Several hospitals across our nation have initiated successful
+                  heart screening programs to detect unknown anomalies. As Heart
+                  Healthy Living starts at home, Chad will showcase recipes
+                  created by Nutritionists and High School Culinary Arts
+                  Programs featuring a Heart Healthy Monthly Recipe. In a
+                  monthly forum, Doctors and Health professionals will speak
+                  about Heart Education and how awareness and prevention are
+                  vital in safeguarding your children’s and your own hearts.
+                  Many parents who have lost a child to SCD tirelessly advocate
+                  for lifesaving tools the AED and CPR so they are available in
+                  every school. Know the Warning Signs and The Chain of
+                  Survival. ‘There is no greater gift than helping to save a
+                  young person’s Life.’
                   <br />
                   <br />
-                  <strong>– “The Gift of Art.”</strong>
+                  <strong>The Gift of Art</strong>
                   <br />
-                  Chad truly cared about all people, as his Friends would say,
-                  Chad never saw color, only you, what you were about, what were
-                  your hopes and dreams, and so through The Gift of Art,” we
-                  create produce/perform/ Art- books, film, theatre, that tell
-                  stories about all our heritages, our dreams and joys and angst
-                  and challenges and promote multi-cultural works and talent.
-                  The Chad Players have performed 9 Off Broadway multi-cultural
-                  performances to challenge the mind and heal the spirit. The
-                  Foundation’s documentary , “Beyond A Mother’s Memories,” shows
-                  the 27 yr. journey to save lives. The Off-Broadway play, “All
-                  About Sneakers,” a play about learning to love oneself, has
-                  been performed for 300 Homeless Men and Women in NYC and moved
-                  many to believe and respect themselves. The Chad Players
-                  production of the classic play,” Days of Wine and Roses,” was
-                  brought to Atlantic City, NJ, sponsored by the “Atlantic
-                  Commission on Abused and Missing Children/Department of Family
-                  and Youth Services” as the community event of the year to show
-                  the deleterious effects substance abuse can have on the
-                  family. We saw first-hand how live theatre affected the teens
-                  as they shouted out, “Don’t take that drink, man!”
+                  Chad truly cared about all people. His Friends would say,
+                  “Chad never saw color, only you, what you were about, what
+                  were your hopes and dreams,” and so
+                  <strong> The Gift of Art </strong>
+                  creates, produces, performs Art—film, theatre, and books that
+                  tell stories about all our heritages, our dreams, joys, angst
+                  and challenges, and promote diverse works and talent. The Chad
+                  Players have performed 9 Off Broadway multi-cultural
+                  performances; their production of the classic play, “Days of
+                  Wine and Roses,” was brought to Atlantic City, NJ, by the
+                  “Atlantic Commission on Abused and Missing Children” as the
+                  community event of the year to show the deleterious effects
+                  substance abuse can have on the family.
                   <br />
                   <br />
-                  Moving on, Chad will focus on children’s “inner hearts,”
-                  endangered as well as the 3 leading causes of death in
-                  children are 11), Firearms, 2), Motor Vehicles, 3), Suicides
-                  and Accidents by providing a platform for high school students
-                  to express their fears, challenges, hopes and dreams through
-                  Art(they create), stories, plays, and athletics so they can
-                  share, express, and inform, and we as parents can listen and
-                  hear their inner heart speaking, effecting a better way for
-                  growth, understanding and solutions. Chad will raise monies
-                  for its Annual Scholarship Programs to award an athlete who
-                  peers honour as the best “Scholar, Athlete & Leader,” and an
-                  “Art Scholarship for the Best Story/Play of the Heart.” As
-                  Chad never drank alcohol or did drugs, we will also launch the
-                  “Life is A Gift – Safe Driving Campaign” to high schools and
-                  colleges--“Don’t text/drink and drive.”
+                  <strong>Moving on</strong>, Chad will focus on children’s
+                  “inner hearts,” as they too are endangered. The 3 leading
+                  causes of death in children are 1) Firearms, 2) Motor
+                  Vehicles, 3) Suicides and Accidents. By providing a platform
+                  for high school students to express their fears, challenges,
+                  hopes and dreams through Art— stories, plays, and athletics
+                  they can share, express, and inform, and we as parents can
+                  listen and hear their inner heart speaking, effecting a better
+                  way for growth, understanding and solutions. Through the
+                  <strong> Annual Scholarship Program</strong>, CHAD awards an
+                  athlete whose peers honour as the best “Scholar, Athlete &amp;
+                  Leader,” and an Art Scholarship for the “Best Story of the
+                  Heart.” As Chad never drank alcohol or did drugs, we will
+                  launch the “Life is A Gift – Safe Driving Campaign” to high
+                  schools and colleges--“Don’t text/drink and drive.”
                   <br />
                   <br />
-                  If we can change - not the landscape of athletics but the
-                  value of each Athlete’s Life through heart screenings, if we
-                  can inspire the mind and heart of just one youth with our Gift
-                  of Art, then we know we have imparted the values of our
-                  namesake, Chad Alan Butrum, “Healthy Body Mind and Spirit.”
-                  Saving one Life to reach their full potential would be Chad’s
-                  greatest wish and joy.
+                  If we can change- not the landscape of athletics but the value
+                  of each Athlete’s Life through heart screenings, if we can
+                  inspire the mind and heart of just one youth with our Gift of
+                  Art, then we know we have imparted the values of our namesake,
+                  Chad Alan Butrum, “Healthy Body Mind and Spirit.” Saving one
+                  Life to reach their full potential would be Chad’s greatest
+                  wish and joy.
                   <br />
                   <br />
                   With your continued Love, Support and Gifts, in the next
@@ -146,12 +124,12 @@ const Message = () => {
                   <br />
                   <br />
                   <span className="message-signature">
-                    From my Heart,
+                    From the Heart,
                     <br />
                     Arista
                     <br />
-                    President and Founder... Chad’s Mom (& Curt's and Collin’s)
-                    the greatest role in Life
+                    Founder... Chad’s Mom (& Curt's and Collin’s) the greatest
+                    role in Life
                   </span>
                 </>
               )}
