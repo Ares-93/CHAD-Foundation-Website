81d0d2030d26b07784cd5485fbe0710965f7acf0
Mohammad Haider <arzhdr@gmail.com>
2024-05-08 16:32:45 -0400

    added 8 tributes

--- my-app/src/components/Pages/5-PeopleWeLove/1-Tributes/2-More-Stories/storiesData.js
+++ my-app/src/components/Pages/5-PeopleWeLove/1-Tributes/2-More-Stories/storiesData.js
@@ -82,6 +82,87 @@ const stories = [
     deathDate: "October 12, 2000",
     age: 15,
   },
+
+  {
+    id: 5,
+    name: "Desiree,Holly,Eric,Kyle,Mitchell",
+    image: require("../../../../../images/PeopleWeLove-Images/Tributes/TheMorrellFamily.jpg"),
+    story: `The Morrell Family was headed by the handsome rogue, Chuck Morrell, former footballer
+    (Washington Redskins) and actor (Banacek). Ironically, just like Chad, who was coming out of a huddle
+    when he collapsed face down, Chuck was running for a touchdown as a WSU Cougar, when he, too,
+    collapsed face-down with no apparent reason – there was no one anywhere near him. But unlike Chad,
+    Chuck got up, having just made the longest run in the Cougar books, 87 yards—fortuitous? Yes, for that
+    moment, a grand record never to be broken only tied and many years later. We lost Chad but Chuck
+    Morrell would lose 5 members of his family to this silent killer, HCM. Today, we know, the HCM gene
+    is found in 1 in 500 births, back then the awareness of the disease was still under the cover of night.
+    First, Chuck lost his baby sister, Michelle, but 3yrs. old to this disease; perhaps thankfully she
+    died in her sleep. Five years later, they would lose their Mom at age 54 to the same heart ailment.
+    Chuck had a twin named , Gary, whose son Kyle, would collapse walking home and subsequently die at
+    age 12. Finally, all were tested and it was found that the twin brothers, Chuck and Gary, also possessed
+    the lethal gene, as well as Gary’s other children, Desiree and Mitchell. Mitchell only two years older
+    than his brother Kyle, would also succumb to the disease just a short while later. Thankfully, the
+    daughter would survive and I would have the pleasure of meeting her son Tyler at the event; he had just
+    received an internal defibrillator, a device that shocks the heart should it go into an erratic arrhythmia so
+    it can start beating at a normal rhythm again. I would also meet Chuck’s lovely daughter, Holly, who
+    would also be diagnosed with HCM and now also has that lifesaving ICD implanted within. In
+    honouring her family, she founded her organization, “Heartfelt Cardiac Programs,” which has been
+    doing community preventive screenings for 15 years. We have held hands at our screening programs and
+    shared the echocardiographic machines thanks to Philips! Holly, too, is part of my ‘Heart Family.’
+    Chuck had a heart transplant in 1995 and always said as he had been given a younger heart, that
+    that’s what kept him young. He had to say goodbye to his twin, Gary, too, who chose not to have a
+    transplant and succumbed to the disease.In 2005, Chuck himself lost the battle but it was hard-fought
+    one just like his daughter Holly would fight when she underwent open-heart surgery recently and
+    survived. It’s important to pay tribute to the pioneers and pillars of our cause. They are who and what
+    give us heart to continue the fight when we get tired-- the stamina to get up and keep doing. Honouring
+    The Morrell Family, the 5 members taken by HCM and those Survivors who devoting their lives to
+    saving hearts.`,
+    deathDate: "October 12, 2000",
+    age: 14,
+  },
+  {
+    id: 6,
+    name: "David Deuel",
+    image: require("../../../../../images/PeopleWeLove-Images/Tributes/David.png"),
+    story: `For where your treasure is there will your heart be also."  His buddies came early, knocking at his window. They rode bikes, played Nintendo 64, and I saw David smiling with his friends as he shot hoops. We always looked forward to Saturday afternoons as we all piled in the car to watch David play basketball. But within two minutes into the game, our perfect lives turned to blackness as we helplessly watched as David suffered a cardiac arrest on the gym floor. If a defibrillator had been on the scene, David could have survived and with treatment, he could have lived a long life.
+    David's Grandma's letter of encouragement to David before his departure: I think you are a "winner" and so does Grandpa. Always remember the song you love "We are the Champions" because it is what you will always be. Coach Rick Hughes: "I could write pages upon pages of what a tremendous young man David was, but I think all I will say is that I have never been associated with anyone with higher integrity, morals, or a more courageous heart."  Mom: The David Deuel Bill has passed here, in Nevada, enabling automated external defibrillators at public facilities without fear of liability to the trained user. Sudden cardiac death is called the "silent epidemic" and much more needs to be done to save lives. We are heartbroken and we miss our David every moment. His enthusiasm for sports and life was contagious. Through Jesus Christ, we know we will be with him again.  "We love you David." "Thank You for a glimpse of Heaven." – Mom (Kathy Deuel) (..more about David and other Sudden Cardiac Victims can be found in the foundation book, “CHAD, A Celebration of Life – Beyond  A Mother’s Memories.”)
+    `,
+    birthDate: "July 13, 1985",
+    deathDate: "February 6, ",
+    age: 14,
+  },
+
+  {
+    id: 7,
+    name: "Kevin Major",
+    image: require("../../../../../images/PeopleWeLove-Images/Tributes/Kevin.png"),
+    story: `Kevin was born on April 8, 1992. His big brown eyes and infectious grin would become his trademark. Kevin started playing hockey at the age of
+    three and played competitively until the time of his passing. As many of Kevin’s teammates would tell you, his motto was “No
+    Excuses, Play Like A Champion”. Off the ice or field, Kevin would live by another motto: “Live Every Day, Laugh
+    Every Moment, Love Beyond Words.” Unfortunately, on July 11, 2011, this physical “big heart” took our son,
+    brother, grandson, and friend from this earth. What we would later discover, Kevin actually had a condition that is
+    called “Hypertrophic Cardiomyopathy (HCM).´ The Kevin J. Major Youth Sports Foundation will sponsor cardiac
+    screenings for youth athletes in our community. Each screening will include an EKG, ECHO cardiogram, cholesterol,
+    and glucose screening along with blood pressure checks, education on CPR, and AEDʼs. The value of these
+    screenings is $3,000 each. Our goal is to help raise awareness of “Sudden Cardiac Arrest” through education,
+    cardiovascular screenings for young athletes and providing AEDs.`,
+    birthDate: "April 8, 1992",
+    deathDate: "July 11, 2011",
+    age: 19,
+  },
+
+  {
+    id: 8,
+    name: "Vanesa Dale",
+    image: require("../../../../../images/PeopleWeLove-Images/Tributes/Vanessa.png"),
+    story: `My baby girl Vanessa was diagnosed at 2 months old with DCM (Dilated Cardiomyopathy) and her struggle for life ended on April 7, 2005. She was only 4 months old but it seems as if she had shared a lifetime with us.
+    While we miss our little angel Vanessa more than we could possibly express in a letter, my husband and I realize that we must choose to either envelop ourselves in self-pity and shy from the world, or latch on to the deeper meaning and a better understanding of the frailty of life on Earth and to embrace even more the remaining moments we have. Due to the support of friends and family, we have clearly chosen the latter and are looking for ways to add meaning and purpose not only to Vanessa's life, but to ours as well. That being said, we joined.
+    cures for cardiomyopathy in children.  I would also like to put my energy into organizing a Free echo-screening.
+    We joined the Children's Cardiomyopathy Foundation (childrenscardiomyopathy.org) and we are dedicated helping uncover causes for children in my area. I live in Houston, TX and I am in close contact with the Texas Children's Hospital Cardiologists which I hope will join me in my efforts. Vanessa's parents have established Vanessa’s Big Heart Foundation to help fund research for pediatric cardiomyopathy.
+    Please visit their web site at http://www.bigheartfoundation.org
+    by Vanessa's mother, Janie`,
+    deathDate: "April 7, 2005",
+    age: 0,
+  },
 ];
 
 export default stories;
