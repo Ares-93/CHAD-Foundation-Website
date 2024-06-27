6d399d3cce5e843ddc001b0b2cd944d2fb81d235
tania <tanakorchynska@gmail.com>
2024-06-27 15:05:49 -0400

    Fixed css issues in the component, made it responsive

---
+++ my-app/src/components/Pages/3-Education of the Heart/1-Heart Facts/1-About-Sudden-Cadiac-Death/AboutCardiacDeath.js
@@ -0,0 +1,78 @@
+import React from "react";
+import EduOfTheHeartNav from "../../EduOfTheHeartNav";
+import "./AboutCardiacDeath.css";
+import { Card, Row, Col } from "react-bootstrap";
+import HeartIcon from "../../../../../images/EduOfHeart-Images/edu-of-heart-icon.png";
+import Heart2 from "../../../../../images/EduOfHeart-Images/HeartModel.png";
+
+const AboutCardiacDeath = () => {
+  return (
+    <section className="HeartFacts">
+      <EduOfTheHeartNav currentPage="heart-facts" />
+      <div className="main-container">
+        <div className="heart-fact-content">
+          <div className="heart-fact-header-box">
+            <img src={HeartIcon} alt="heart" width={60} height={60} />
+            <h2 className="heart-fact-header">About Sudden Cardiac Death</h2>
+          </div>
+          {/* CARDS */}
+          <Row xs={1} md={1} lg={3} className="g-4 justify-content-center">
+            {/* Card-1 */}
+            <Col>
+              <Card className="heart-fact-card">
+                <Card.Body>
+                  <img src={Heart2} alt="Heart" className="heart-fact-icon" />
+                  <div className="heart-fact-card-title-box">
+                    <Card.Title className="heart-fact-card-title">
+                      180K - 300K
+                    </Card.Title>
+                  </div>
+                  <Card.Text className="heart-fact-card-text">
+                    Sudden Cardiac Deaths (SCD) occur in the US annually <br />
+                    <span className="card-text-transparent">lorem ipsum</span>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            {/* Card-2 */}
+            <Col>
+              <Card className="heart-fact-card">
+                <Card.Body>
+                  <img src={Heart2} alt="Heart" className="heart-fact-icon" />
+                  <div className="heart-fact-card-title-box">
+                    <Card.Title className="heart-fact-card-title">
+                      1 IN 500
+                    </Card.Title>
+                  </div>
+                  <Card.Text className="heart-fact-card-text">
+                    people have the Hypertrophic Cardiomyopathy (HCM) abnormal
+                    gene <br />
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            {/* Card-3 */}
+            <Col>
+              <Card className="heart-fact-card">
+                <Card.Body>
+                  <img src={Heart2} alt="Heart" className="heart-fact-icon" />
+                  <div className="heart-fact-card-title-box">
+                    <Card.Title className="heart-fact-card-title">
+                      5000+
+                    </Card.Title>
+                  </div>
+                  <Card.Text className="heart-fact-card-text">
+                    young people between 15 and 34 years of age die annually
+                    from Sudden Cardiac Death
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutCardiacDeath;
