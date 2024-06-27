2b577b21600dd51aabc8956a7f9ccd8576d86b43
tania <tanakorchynska@gmail.com>
2024-06-27 15:06:07 -0400

    Update ScreenHome.js

--- my-app/src/components/Pages/3-Education of the Heart/2-Screenings/0-Screening-Home/ScreenHome.js
+++ my-app/src/components/Pages/3-Education of the Heart/2-Screenings/0-Screening-Home/ScreenHome.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Stats from "../1-Statistics/Stats";
-import Importance from "../2-Importance/Importance";
+import Importance from "../2-Importance/Importance"; //THIS COMPONENT BREAKS THE PAGE
 import HSS from "../3-Heart-Screen-Stats/HSS";
 
 const ScreenHome = () => {
   return (
     <section className="ScreeningPage">
       <Stats />
-      <Importance />
+      <Importance /> 
       <HSS />
     </section>
   );
