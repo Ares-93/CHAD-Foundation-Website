0ae16c182cc457f16f5a56fde5c62c2b8738fba3
tania <tanakorchynska@gmail.com>
2024-06-27 15:06:53 -0400

    Fixed css and responsiveness issues in the component; added one more button of the gallery

--- my-app/src/components/Pages/3-Education of the Heart/2-Screenings/1-Statistics/Stats.js
+++ my-app/src/components/Pages/3-Education of the Heart/2-Screenings/1-Statistics/Stats.js
@@ -9,37 +9,33 @@ import { useNavigate } from "react-router-dom";
 const Stats = () => {
   const navigate = useNavigate();
 
+  const handleGalleryClick = () => {
+    navigate("/screenings-gallery");
+  };
+
   const handleExploreClick = () => {
     navigate("/resources/sca-and-training-resources");
   };
 
   return (
-    <section className="stats">
+    <section className="StatsSection">
       <EduOfTheHeartNav currentPage="screenings" />
       <div className="main-container">
         <div className="stats-header-box">
-          {<img src={HeartIcon} alt="doc-heart" width={60} height={60} />}
+          <img src={HeartIcon} alt="doc-heart" width={60} height={60} />
           <h2 className="stats-header">Statistics Of Our Heart Screenings</h2>
         </div>
         <div className="stats-content">
-          <p className="stats-p"></p>
-          {/* CARDS */}
           <Row xs={1} md={1} lg={3} className="g-4 justify-content-center">
             {/* Card-1 */}
             <Col>
               <Card className="stats-card">
                 <Card.Body>
-                  <img
-                    src={DocHeart}
-                    alt="Heart-2"
-                    className="whatYouShouldKnow-icon"
-                  />
-                  <div className="whatYouShouldKnow-card-title-box">
-                    <Card.Title className="whatYouShouldKnow-card-title">
-                      10,000
-                    </Card.Title>
+                  <img src={DocHeart} alt="Heart-2" className="stats-icon" />
+                  <div className="stats-card-title-box">
+                    <Card.Title className="stats-card-title">10,000</Card.Title>
                   </div>
-                  <Card.Text className="whatYouShouldKnow-card-text">
+                  <Card.Text className="stats-card-text">
                     people chose to have CHAD Heart Screenings. See 22 years of
                     Chad Preventive Heart Screenings!
                     <br />
@@ -51,20 +47,17 @@ const Stats = () => {
             <Col>
               <Card className="stats-card">
                 <Card.Body>
-                  <img
-                    src={DocHeart}
-                    alt="DocHeart"
-                    className="whatYouShouldKnow-icon"
-                  />
-                  <div className="whatYouShouldKnow-card-title-box">
-                    <Card.Title className="whatYouShouldKnow-card-title">
-                      10-20%
-                    </Card.Title>
+                  <img src={DocHeart} alt="DocHeart" className="stats-icon" />
+                  <div className="stats-card-title-box">
+                    <Card.Title className="stats-card-title">10-20%</Card.Title>
                   </div>
                   <Card.Text className="stats-card-text">
-                    of athletes screened had heart anomalies detected.
+                    of athletes screened had heart anomalies detected
                     <br />
-                    <span className="card-text-transparent">lorem ipsum</span>
+                    <span className="stats-card-text-transparent">
+                      lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem
+                      ipsum
+                    </span>
                   </Card.Text>
                 </Card.Body>
               </Card>
@@ -73,40 +66,42 @@ const Stats = () => {
             <Col>
               <Card className="stats-card">
                 <Card.Body>
-                  <img
-                    src={DocHeart}
-                    alt="DocHeart"
-                    className="whatYouShouldKnow-icon"
-                  />
-                  <div className="whatYouShouldKnow-card-title-box">
-                    <Card.Title className="whatYouShouldKnow-card-title">
-                      1 in 3
-                    </Card.Title>
+                  <img src={DocHeart} alt="DocHeart" className="stats-icon" />
+                  <div className="stats-card-title-box">
+                    <Card.Title className="stats-card-title">1 in 3</Card.Title>
                   </div>
-                  <Card.Text className="whatYouShouldKnow-card-text">
+                  <Card.Text className="stats-card-text">
                     students were at risk for hypertension, obesity, diabetes,
-                    high cholesterol, and/or heart abnormalities.
+                    high cholesterol, and/or heart abnormalities
+                    <br />
+                    <span className="stats-card-text-transparent">
+                      lorem ipsum
+                    </span>
                   </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
         </div>
-        <div className="link">
-          <a href="/more-stats" className="stats-link">
-            To see pictures of CHAD Heart Screenings at work, click here.
-          </a>
-        </div>
-
-        <button className="find-screen-btn" onClick={handleExploreClick}>
-          Find Heart Screenings Near You
-        </button>
-
-        <div className="expert-text">
+        <div className="stats-expert-text">
           Experts now say that community awareness, implementation of
           cardiovascular screening programs, knowledge of CPR and AED placement
           can safeguard young hearts, and help to prevent Sudden Cardiac Death.
-          <br></br>
+          <br />
+        </div>
+        <div className="stats-gallery-btns">
+          <button
+            className="stats-gallery-btn screenings-btn"
+            onClick={handleGalleryClick}
+          >
+            Gallery - CHAD Heart Screenings
+          </button>
+          <button
+            className="stats-gallery-btn explore-btn"
+            onClick={handleExploreClick}
+          >
+            Find Heart Screenings Near You
+          </button>
         </div>
       </div>
     </section>
