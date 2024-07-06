4d3af1437d781e47ddf70a0ef8c03557fcabecaa
Mohammad Haider <arzhdr@gmail.com>
2024-07-06 12:16:25 -0400

    updated: Mack Hale, Mackenzie Gray, Michaela, Troy

--- my-app/src/components/Pages/5-PeopleWeLove/2-Survivors/2-More-Survivor-Stories/SurvivorStoriesData.js
+++ my-app/src/components/Pages/5-PeopleWeLove/2-Survivors/2-More-Survivor-Stories/SurvivorStoriesData.js
@@ -45,41 +45,106 @@ const survivorStories = [
     `,
     job: `Child Survivor`,
   },
+
   {
     id: 3,
     name: "Mack Hale",
     image: require("../../../../../images/PeopleWeLove-Images/Survivors/Mack.jpg"),
     story: `
-    When Mack took the mound in the bottom of the first, his calm demeanor turned to nervousness, making that first-pitch strike on the outside part of the plate harder than it looked. This happens all the time. "Right before the game starts, right before the first pitch, it's pounding."  Mack was referring to his heart, which beats a little faster and a little harder when the umpire calls for the initial pitch. There was a chance Hale might not have had the opportunity to make that pitch this year. Last October, during a heart screening at Holyoke High School performed by The Chad Foundation, Hale learned he suffered from a dilated left ventricle. More visits with doctors followed at Baystate Medical Center in Springfield, the cardiology clinic at Children's Hospital Boston in Waltham and Boston Children's Hospital. With each appointment came an assurance that Hale was OK and he could play sports.
-Mack planned a baseball derby to raise funds to bring a CHAD screening for the athletes of his own Northampton High School. CHAD was delighted to provide a screening at his high school for all this teammates. A few years ago, Mack graduated Magna Cum Laude from Middlebury College (VT) with a degree in Molecular Biology & Biochemistry with departmental honors. “Cardiology is definitely a huge interest for me. It's something I saw a lot of when I was undergoing all of the testing in high school for my enlarged left ventricle. Some type of surgery is also a major interest of mine.”
+      When Mack took the mound in the bottom of the first, his calm demeanor turned to nervousness,
+      making that first-pitch strike on the outside part of the plate harder than it looked. This happens all the time. "Right before the game starts, right before the first pitch, it's pounding." Mack was
+      referring to his heart, which beats a little faster and a little harder when the umpire calls for the
+      initial pitch. There was a chance Hale might not have had the opportunity to make that pitch this
+      year. Last October, he and his family braved the nor’easter to be the last one to receive a heart
+      screening at Holyoke High School performed by The Chad Foundation, Hale learned he suffered
+      from a dilated left ventricle. More visits with doctors followed at Baystate Medical Center in
+      Springfield, the cardiology clinic at Children's Hospital Boston in Waltham, and Boston
+      Children's Hospital. With each appointment came an assurance that Hale was OK and he could
+      play sports. Mack planned a baseball derby to raise funds to bring a CHAD screening for the
+      athletes of his own Northampton High School. CHAD was delighted to provide a screening at his
+      high school for all his teammates. A few years ago, Mack graduated Magna Cum Laude from
+      Middlebury College (VT) with a degree in Molecular Biology & Biochemistry with departmental
+      honors. “Cardiology is definitely a huge interest for me. It's something I saw a lot of when I was
+      undergoing all of the testing in high school for my enlarged left ventricle. Some type of surgery
+      is also a major interest of mine.”
     `,
     job: `Survivor – Pursuing a Career in Medicine`,
   },
+
   {
     id: 4,
     name: "Mackenzie Gray",
     image: require("../../../../../images/PeopleWeLove-Images/Survivors/Mackenzie.jpg"),
     story: `
-    When Mackenzie’s brother returned home from college, January 5, 2012, Mack recounts, ‘in an effort to stay true to his commitment to the team, Jonathan was completing an hour-long rowing workout on an ergometer he brought home from school when he experienced a severe and deadly arrhythmia. Attempts by his brother, Mom, and EMTs failed to revive him. Doctors and pathologists surmise that he died of a probable arrhythmia from an unknown cause.’   
-    Can the light shine through such sadness? Jonno’s 22-year-old brother, Mack, says, yes. Because of the unknown cause of his brother, Jonathan’s arrhythmia and the potential for genetic conditions, Mackenzie was screened with an EKG, which detected an anomaly. An Echocardiogram determined an enlarged and mildly dilated right ventricle. Doctors initially believed that Mac suffered from a condition called ARVD, which can cause deadly arrhythmias with vigorous exercise- certainly a threat for a serious runner. A second Echocardiogram revealed an Atrial Septal Defect, which was responsible for the enlargement. As a 7-time marathoner and generally active person, Mac was stunned to learn that he had a nickel-sized hole in his heart without any symptoms. Mac credits his brother Jonathan with saving his Life. Mackenzie underwent open-heart surgery on April 19, 2013, and was able to return to running without the fear of a stroke, right ventricular failure, or death caused by the defect. He is a survivor because of cardiac screening.’
-    CHAD came back in 2012 and 2013 to provide heart screenings at Holyoke High School, MA in special tribute of Mac’s brother, Jonno Gray.
-    `,
+    When Mackenzie’s brother returned home from college, January 5, 2012, Mack recounts, ‘in an
+    effort to stay true to his commitment to the team, Jonathan was completing an hour-long rowing
+    workout on an ergometer he brought home from school when he experienced a severe and deadly
+    arrhythmia. Attempts by his brother, Mom, and EMTs failed to revive him. Doctors and
+    pathologists surmise that he died of a probable arrhythmia from an unknown cause.’ Can the
+    light shine through such sadness? Jonno’s 22-year-old brother, Mack, says, yes. Because of the
+    unknown cause of his brother Jonathan’s arrhythmia and the potential for genetic conditions,
+    Mackenzie was screened with an EKG, which detected an anomaly. An Echocardiogram
+    determined an enlarged and mildly dilated right ventricle. Doctors initially believed that Mac
+    suffered from a condition called ARVD, which can cause deadly arrhythmias with vigorous
+    exercise- certainly a threat for a serious runner. A second Echocardiogram revealed an Atrial
+    Septal Defect, which was responsible for the enlargement. As a 7-time marathoner and generally
+    active person, Mac was stunned to learn that he had a nickel-sized hole in his heart without any
+    symptoms. He was a walking time-bomb. Mac credits his brother Jonathan with saving his Life.
+    Mackenzie underwent open-heart surgery on April 19, 2013, and was able to return to running
+    without the fear of a stroke, right ventricular failure, or death caused by the defect. He is a
+    survivor because of cardiac screening.’ CHAD came back in 2012 and 2013 to provide heart
+    screenings at Holyoke High School, MA in special tribute of Mac’s brother, Jonno Gray.
+  `,
     job: `Runner`,
   },
+
   {
     id: 5,
     name: "Michaela Gagne",
     image: require("../../../../../images/PeopleWeLove-Images/Survivors/Michaela.jpg"),
     story: `
-    Michaela was crowned Miss Massachusetts in June of 2006 and went on to compete for the title of Miss America 2007. This title furthered her continuing work entitled Heart Health: Listen, Learn, and Live, which was a reaction to having been diagnosed with a life-threatening heart condition and having undergone surgery for an implantable cardioverter defibrillator at the age of 17. She is currently a national spokesperson for the American Heart Association’s Go Red for Women Campaign, and she is a frequent speaker/lobbyist on Capitol Hill, as well as a requested speaker across the United States and Canada. Despite her heart condition, Long QT Syndrome, Michaela is an accomplished athlete, competing regularly in soccer and basketball, and is a MIAA certified high school track and field coach. She is a certified CPR/AED trainer. Honouring Children – Survivors Learn about heart screenings for you and your family. You may have no symptoms and no family history of heart disease or Sudden Cardiac Arrest, but neither do many victims of SCA. Simple cardiac tests, including Electrocardiograms (ECG’s) and Echocardiograms, are capable of discovering a wealth of information about your heart…examinations that are important for everyone. Many young people have underlying heart conditions with no symptoms `,
+    Michaela was crowned Miss Massachusetts in June of 2006 and went on to compete for the title
+    of Miss America 2007. This title furthered her continuing work entitled Heart Health: Listen,
+    Learn, and Live, which was a reaction to having been diagnosed with a life-threatening heart
+    condition and having undergone surgery for an implantable cardioverter defibrillator at the age of
+    17. She is currently a national spokesperson for the American Heart Association’s Go Red for
+    Women Campaign, and she is a frequent speaker/lobbyist on Capitol Hill, as well as a requested
+    speaker across the United States and Canada. Despite her heart condition, Long QT Syndrome,
+    Michaela is an accomplished athlete, competing regularly in soccer and basketball, and is a
+    MIAA certified high school track and field coach. She is a certified CPR/AED trainer.
+    Honouring Children – Survivors Learn about heart screenings for you and your family. You may
+    have no symptoms and no family history of heart disease or Sudden Cardiac Arrest, but neither
+    do many victims of SCA. Simple cardiac tests, including Electrocardiograms (ECG’s) and
+    Echocardiograms, are capable of discovering a wealth of information about your heart
+    examinations that are important for everyone. Many young people have underlying heart
+    conditions with no symptoms.
+  `,
     job: `Survivor & Professional Speaker on Heart Health Advocacy`,
   },
+
   {
     id: 6,
     name: "Troy Wirth",
     image: require("../../../../../images/PeopleWeLove-Images/Survivors/Troy.jpg"),
     story: `
-    “I have been meaning to write and thank you for all the spectacular work The Chad Foundation has been doing across the country. I am especially appreciative because I am one of the lives you might have saved. Due to your echocardiogram screening at the Chad Foundation Malibu Benefit for Children’s Hospital Los Angeles, doctors detected an arrhythmia and advised immediate follow-up which I did and ended up having surgery the following month in October, with a diagnosis of Wolf-Parkinson-White Syndrome. All my life I suffered from this undiagnosed physical challenge and played NCAA Division 1 volleyball at USC and never could play to my optimum. Now, I am playing sports better than ever thanks to your fortuitous screening event. I would be honored to be a spokesperson for your cause, of which I am living proof!”`,
+      “I have been meaning to write and thank you for all the spectacular work The Chad Foundation
+      has been doing across the country. I am especially appreciative because I am one of the lives you
+      might have saved. Due to your echocardiogram screening at the Chad Foundation Malibu Benefit
+      for Children’s Hospital Los Angeles, doctors detected an arrhythmia and advised immediate
+      follow-up which I did and ended up having surgery the following month in October, with a
+      diagnosis of Wolf-Parkinson-White-Syndrome. All my life I suffered from this undiagnosed
+      physical challenge and played NCAA Division 1 volleyball at USC and never could play to my
+      optimum. Now, I am playing sports better than ever thanks to your fortuitous screening event. I
+      would be honored to be a spokesperson for your cause, of which I am living proof!”
+  
+      -Troy Wirth
+  
+      Owner,
+  
+      Orange Coast Insurance Solutions
+  
+      Irvine, California
+    `,
     job: `Vice President of Metropolitan Life`,
   },
 ];
