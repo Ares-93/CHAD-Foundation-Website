acef9a1bf2e0f0f941ae9bc37ab71a225700485f
Mohammad Haider <arzhdr@gmail.com>
2024-07-06 11:46:37 -0400

    added readmore functionality to the survivor stories

--- my-app/src/components/Pages/5-PeopleWeLove/2-Survivors/2-More-Survivor-Stories/Survivor-Stories-Modal.js
+++ my-app/src/components/Pages/5-PeopleWeLove/2-Survivors/2-More-Survivor-Stories/Survivor-Stories-Modal.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Surivor-Stories-Modal.css";
 
 const SurvivorStoryModal = ({ story, onClose }) => {
+  const [showFullText, setShowFullText] = useState(false);
+
+  const handleReadMoreToggle = () => {
+    setShowFullText(!showFullText);
+  };
+
+  const shortStory = story.story.split(" ").slice(0, 50).join(" ") + "...";
+
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
@@ -14,7 +22,12 @@ const SurvivorStoryModal = ({ story, onClose }) => {
           {story.birthDate} <br />
         </p>
         <h2 className="modal-curr-life">{story.job}</h2>
-        <p className="modal-text">{story.story}</p>
+        <p className="modal-text">
+          {showFullText ? story.story : shortStory}
+          <button className="readMore-btn" onClick={handleReadMoreToggle}>
+            {showFullText ? "Show Less" : "Read More"}
+          </button>
+        </p>
       </div>
     </div>
   );
