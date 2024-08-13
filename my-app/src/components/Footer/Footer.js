4811a3383608bb1c1f45969dcb8d5ad2ce1c7145
Mohammad Haider <arzhdr@gmail.com>
2024-08-13 02:44:36 -0400

    Revert "Merge branch 'footer-links'"
    This reverts commit 0fbee3c8b917c1efa85782f0f9e5f2f2f51f0dbf, reversing
    changes made to 1c935d0bff8aaca8abf30fdbd0d532a7cd8fadc5.

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -95,30 +95,10 @@ function Footer() {
           {/* Education of the Heart */}
           <Col md className="footer-col">
             <h3 className="footer-h3">Education of the Heart</h3>
-            <ScrollToTopNavLink
-              to="/education-of-the-heart/heart-fact-page"
-              className="footer-p nav-link"
-            >
-              Heart Facts
-            </ScrollToTopNavLink>
-            <ScrollToTopNavLink
-              to="/education-of-the-heart/screening-home"
-              className="footer-p nav-link"
-            >
-              Screenings
-            </ScrollToTopNavLink>
-            <ScrollToTopNavLink
-              to="/education-of-the-heart/emergency-main"
-              className="footer-p nav-link"
-            >
-              Emergencies
-            </ScrollToTopNavLink>
-            <ScrollToTopNavLink
-              to="/education-of-the-heart/tips-main"
-              className="footer-p nav-link"
-            >
-              Expert Health Tips
-            </ScrollToTopNavLink>
+            <p className="footer-p">Heart Facts</p>
+            <p className="footer-p">Screenings</p>
+            <p className="footer-p">Emergencies</p>
+            <p className="footer-p">Expert Health Tips</p>
           </Col>
           {/* Gift of Art */}
           <Col md className="footer-col">
@@ -163,18 +143,8 @@ function Footer() {
           {/* People We Love */}
           <Col md className="footer-col">
             <h3 className="footer-h3">People We Love</h3>
-            <ScrollToTopNavLink
-              to="/people-we-love/tributes"
-              className="footer-p nav-link"
-            >
-              Tributes
-            </ScrollToTopNavLink>
-            <ScrollToTopNavLink
-              to="/people-we-love/survivors"
-              className="footer-p nav-link"
-            >
-              Survivors
-            </ScrollToTopNavLink>
+            <p className="footer-p">Tributes</p>
+            <p className="footer-p">Survivors</p>
             <h3 className="footer-h3">Donate</h3>
           </Col>
         </Row>
