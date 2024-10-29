f032eb383f947482c06aefc969e9cd24b7ac148f
tania <tanakorchynska@gmail.com>
2024-10-28 21:44:22 -0400

    Added a section of the Donate page

---
+++ my-app/src/components/Pages/7-DonatePage/1-DonateSection/DonateSection.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./DonateSection.css";
+
+const DonateSection = () => {
+  return (
+    <section className="DonateSection">
+      <div className="main-container">
+        <h2 className="donate-page-header">Donate to Save Young Lives</h2>
+        <p className="donate-page-p">
+          Together, we can make a difference and save lives.{" "}
+        </p>
+        <p className="donate-page-p">
+          Please donate today and help us raise awareness of this important
+          issue.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default DonateSection;
