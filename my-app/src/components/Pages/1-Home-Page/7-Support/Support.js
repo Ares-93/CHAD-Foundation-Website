88cafa5001c99fabc591fcc1a5a8829d3e4dfe64
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-06-05 13:05:17 -0400

    Update Support.js

--- my-app/src/components/Pages/1-Home-Page/7-Support/Support.js
+++ my-app/src/components/Pages/1-Home-Page/7-Support/Support.js
@@ -9,7 +9,7 @@ const Support = () => {
   return (
     <section className="Support">
       <div className="main-container">
-        <h2>Support the Chad Foundation:</h2>
+        <h2>The Chad Foundation Memoir Book:</h2>
         <div className="content">
           <div className="book-display">
             <img className="book-chad" src={Books} alt="Book" />
@@ -19,17 +19,18 @@ const Support = () => {
             <p>
               Celebrating the lives of young athletes lost to Sudden Cardiac
               Death and the journey of its survivors, this uplifting memoir
-              details the 25-year history of The Chad Foundation to safeguard
-              hearts through 10,000 echocardiogram and EKG screenings, shares
-              heartfelt stories of other Families who have lost children to SCD,
+              details the 25-year history of The Chad Foundation for Athletes
+              and Artists to safeguard hearts by providing 10,000 Echocardiogram
+              and EKG screenings in 5 states, and Austria, and Sweden. It shares
+              heartfelt stories of other Families who have lost children to SCD
               and presents the history and importance of heart screenings—its
-              pros and cons— through scientific studies. Endorsed by Adam
-              Silver, Commissioner of the National Basketball Association, CHAD
-              celebrates Life through awareness and advocation of preventive
-              screenings, AEDs and CPR. From SCD victims to survivors — from
-              heartbreak to heroism — learn how The Chad Foundation and
-              countless advocate organizations, and cardiologists have
-              safeguarded the lives of youth so they will have a better future.
+              pros and cons— through scientific studies, and the lifesaving
+              tools—AEDs and CPR. The book has the endorsement of Adam Silver,
+              Commissioner of the National Basketball Association. From Sudden
+              Cardiac Victims to Survivors — heartbreak to heroism — learn how
+              The Chad Foundation and countless advocate organizations, and
+              cardiologists and health professionals worldwide have safeguarded
+              the lives of youth so they will have a better future.
             </p>
             <button onClick={() => navigate("/gift-of-art/books")}>
               CHAD BOOK EXCERPTS
