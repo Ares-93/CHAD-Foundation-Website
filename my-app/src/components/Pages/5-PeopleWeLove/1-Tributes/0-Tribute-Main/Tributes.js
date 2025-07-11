6330389e1ef931c8baa7c14a2f0f6b52959aa917
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-07-11 19:55:53 -0400

    Update Tributes.js

--- my-app/src/components/Pages/5-PeopleWeLove/1-Tributes/0-Tribute-Main/Tributes.js
+++ my-app/src/components/Pages/5-PeopleWeLove/1-Tributes/0-Tribute-Main/Tributes.js
@@ -3,14 +3,17 @@ import PeopleNav from "../../PeopleNav";
 import HonoringLives from "../1-HonoringLives/HL";
 import StoriesGrid from "../2-More-Stories/MS";
 import NeverForget from "../3-NeverForget/NeverForget";
+import "./Tributes.css";
 
 const Tributes = () => {
   return (
     <section className="Tributes">
       <PeopleNav currentPage="tributes" />
-      <HonoringLives />
-      <StoriesGrid />
-      <NeverForget />
+      <div className="Tributes-sections">
+        <HonoringLives />
+        <StoriesGrid />
+        <NeverForget />
+      </div>
     </section>
   );
 };
